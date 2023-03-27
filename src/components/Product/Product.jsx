a29c101dba2c97718919b0af1053555c578b9f1e
akashislam1 <m.naimul110@gmail.com>
2023-03-27 20:41:07 +0600

    add

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Product = (props) => {
   //   console.log(props.product);
+  const handleAddToCart = props.handleAddToCart;
   const { img, name, price, ratings, seller } = props.product;
   return (
     <>
@@ -17,12 +18,31 @@ const Product = (props) => {
           <p className="font-semibold text-xl">Price : ${price}</p>
           <div>
             <p>Manufacturer : {seller}</p>
-            <p>Rating : {ratings}</p>
+            <p>Rating : {ratings} Stars</p>
           </div>
         </div>
 
-        <button className="w-100 bg-amber-300 hover:bg-orange-400 p-2 text-center rounded-b-md">
+        <button
+          onClick={() => handleAddToCart(props.product)}
+          className="w-100 flex justify-center gap-1 font-semibold bg-amber-300 hover:bg-orange-400 p-2 text-center rounded-b-md"
+        >
           Add to Cart
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </span>
         </button>
       </div>
     </>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,12 @@ import Product from "../Product/Product";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (product) => {
+    const neqCart = [...cart, product];
+    setCart(neqCart);
+  };
 
   useEffect(() => {
     fetch("products.json")
@@ -11,13 +17,20 @@ const Shop = () => {
   }, []);
   return (
     <>
-      <div className="px-20 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 my-8">
+      <div className="px-20  grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 my-8">
         <div className="lg:col-span-3 grid lg:grid-cols-3 gap-5">
           {products?.map((product) => (
-            <Product key={product.id} product={product}></Product>
+            <Product
+              key={product.id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            ></Product>
           ))}
         </div>
-        <div>Order Summary</div>
+        <div className="text-center bg-amber-200 ">
+          <h2 className="my-5 text-2xl font-bold ">Order Summary</h2>
+          <p className="font-semibold">Selected Items: {cart.length}</p>
+        </div>
       </div>
     </>
   );
